61f209cf120b03c2f447087a7c7c3d05b23166c6
nitstrange <sajib.ahasan@hotmail.com>
2020-03-22 04:41:25 +0600

    uwsgi setting

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import connexion
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./')
-application = app.app
+
 #add swagger yaml from connexion module
 app.add_api('swagger.yml')
 #home route
@@ -17,4 +17,5 @@ def home():
 
 #running flask in standalone mode
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
+    application = app.app
